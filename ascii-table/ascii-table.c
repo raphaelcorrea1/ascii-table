fcbcd38bafc41e0798bd0b286bdb1cbfc69d1106
raphaelcorrea1 <65843885+raphaelcorrea1@users.noreply.github.com>
2024-02-02 04:29:29 -0300

    deleting wrong test snippet

--- ascii-table/ascii-table.c
+++ ascii-table/ascii-table.c
@@ -18,10 +18,6 @@ int main(void){
     for (int i = 64; i < 128; i++){
         printf("   %d          %c           %x\n", i, i, i);
     }
-
-    char *info = "Web Developer and Cyber Security Student";
-    char *langs[] = {"Java", "Java SpringBoot", "JPA/Hibernate", "PHP", "C"};
-    char *areas[] = {"Web Development", "Software Development", "Pentest", "Red Team"};
     
     return 0;
 }
